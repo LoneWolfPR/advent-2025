85808f1185a4e0deb868974a872262d6502f5a9e
lonewolfpr <paul.n.romer@gmail.com>
2025-12-16 18:07:57 -0500

    day 10 part 1

--- day10/part1.go
+++ day10/part1.go
@@ -2,9 +2,12 @@ package day10
 
 import (
 	"fmt"
+	"os"
+	"slices"
 
 	"github.com/lonewolfpr/advent-2025/utils"
 )
+
 func Part1() bool {
 	diagramFile, err := GatherInput()
 	if err != nil {
@@ -12,15 +15,101 @@ func Part1() bool {
 		return utils.RunAnotherPuzzlePrompt()
 	}
 
-	machines := ingestDiagramFile(diagramFile)
+	machines, err := ingestDiagramFile(diagramFile)
+	if err != nil {
+		fmt.Printf("Error ingesting diagram: %v", err)
+		return utils.RunAnotherPuzzlePrompt()
+	}
 	buttonPresses := calcFewestButtonPresses(machines)
 
 	fmt.Printf("The fewest button presses required to start all machines is: %d", buttonPresses)
 	return utils.RunAnotherPuzzlePrompt()
 }
 
-func calcFewestButtonPresses([]MachineInfo) int {
+func calcFewestButtonPresses(machines []MachineInfo) int {
 	buttonPresses := 0
+	logFile, err := os.Create("./day10/log.txt")
+	if err != nil {
+		panic("can't open log file")
+	}
 
+	for index, machine := range machines {
+		machinePresses := calcFewestButtonPressesForMachine(machine)
+		fmt.Fprintf(logFile, "Presses for line %d: %d\n", index + 1, machinePresses)
+		buttonPresses += machinePresses
+	}
+	defer logFile.Close()
 	return buttonPresses
+}
+
+func calcFewestButtonPressesForMachine(machine MachineInfo) int {
+	buttonPresses := 0
+	//checkedStates := map[string]bool{} // convert list of clicked button indexes to a string for the key
+	// set a slice to keep track of number of times a light is toggled
+	// create initial slice of all button indexes
+	allButtons := []int{}
+	for index := range machine.buttons {
+		allButtons = append(allButtons, index)
+	}
+	allButtonsStr := fmt.Sprintf("%v", allButtons)
+	cache := []string{allButtonsStr}
+	allCombinations := buildButtonCombinations(allButtons, &cache)
+	// the combination of all buttons needs to be added as well
+	allCombinations = append(allCombinations, allButtons)
+	
+	for _, combination := range allCombinations {
+		currState := make([]int, len(machine.requiredPattern))
+		for _, button := range combination {
+			for _, light := range machine.buttons[button] {
+				currState[light]++
+			}
+		}
+		valid := checkState(machine.requiredPattern, currState)
+		if valid && (buttonPresses == 0 || len(combination) < buttonPresses) {
+			buttonPresses = len(combination)
+		}
+	}
+	return buttonPresses
+}
+
+func checkState(requiredPattern []int, state []int) bool {
+	for index, expected := range requiredPattern {
+		lightState := state[index] % 2
+		if expected != lightState{
+			return false
+		}
+	}
+	return true
+}
+
+func buildButtonCombinations(allButtons []int, cache *[]string) [][]int {
+	combinations := [][]int{}
+	for index := range allButtons {
+		delIndex := len(allButtons) - index - 1
+		combination := removeButton(allButtons, delIndex)
+		combinationStr := fmt.Sprintf("%v",combination)
+		if slices.Contains(*cache, combinationStr) {
+			continue
+		}
+		combinations = append(combinations, combination)
+		*cache = append(*cache, combinationStr)
+		if len(combination) > 1 {
+			childCombinations := buildButtonCombinations(combination, cache)
+			combinations = append(combinations, childCombinations...)
+		} else {
+			continue
+		}
+	}
+	return combinations
+}
+
+func removeButton(buttons []int, indexToRemove int) []int {
+	subset := []int{}
+	for i := 0; i < len(buttons); i++ {
+		if i == indexToRemove {
+			continue
+		}
+		subset = append(subset, buttons[i])
+	}
+	return subset
 }
--- day10/utils.go
+++ day10/utils.go
@@ -1,8 +1,11 @@
 package day10
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type MachineInfo struct {
@@ -26,8 +29,86 @@ func GatherInput() (*os.File, error) {
 	return diagramFile, nil
 }
 
-func ingestDiagramFile(file *os.File) []MachineInfo {
+func ingestDiagramFile(file *os.File) ([]MachineInfo, error) {
 	machines := []MachineInfo{}
 
-	return machines
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		machine := MachineInfo{}
+		line := strings.TrimSpace(scanner.Text())
+		pieces := strings.Split(line, " ")
+		for _, piece := range pieces {
+			switch string(piece[0]) {
+			case "[":
+				pattern, err := getRequiredPattern(piece)
+				if err != nil {
+					return nil, fmt.Errorf("Error reading pattern: %v", err)
+				}
+				machine.requiredPattern = pattern
+			case "(":
+				button, err := buildButton(piece)
+				if err != nil {
+					return nil, fmt.Errorf("Error building button: %v", err)
+				}
+				machine.buttons = append(machine.buttons, button)
+			case "{":
+				joltages, err := gatherJoltages(piece)
+				if err != nil {
+					return nil, fmt.Errorf("Error gathering joltages: %v", err)
+				}
+				machine.joltageRequirements = joltages
+			}
+		}
+		machines = append(machines, machine)
+	}
+
+	return machines, nil
+}
+
+func getRequiredPattern(patternStr string) ([]int, error) {
+	requiredPattern := []int{}
+	cleanPattern := strings.Trim(patternStr, "[]")
+	for _, char := range cleanPattern {
+		switch string(char){
+		case ".":
+			requiredPattern = append(requiredPattern, 0)
+		case "#":
+			requiredPattern = append(requiredPattern, 1)
+		default:
+			return nil, fmt.Errorf("Invalid character in pattern: %s", string(char))
+		}
+	}
+
+	return requiredPattern, nil
+}
+
+func buildButton(btnConfigStr string) ([]int, error) {
+	btnConfig := []int{}
+	cleanConfig := strings.Trim(btnConfigStr, "()")
+	lights := strings.Split(cleanConfig, ",")
+	for _, light := range lights {
+		lightIndex, err := strconv.Atoi(light)
+		if err != nil {
+			return nil, fmt.Errorf("light index is not valid: %s", light)
+		}
+		btnConfig = append(btnConfig, lightIndex)
+	}
+
+	return btnConfig, nil
+}
+
+func gatherJoltages(joltagesStr string) ([]int, error) {
+	joltages := []int{}
+	cleanJoltagesStr := strings.Trim(joltagesStr, "{}")
+	joltageStrs := strings.Split(cleanJoltagesStr, ",")
+	for _, joltageStr := range joltageStrs {
+		joltage, err := strconv.Atoi(joltageStr)
+		if err != nil {
+			return nil, fmt.Errorf("Joltage is not valid: %d", joltage)
+		}
+		joltages = append(joltages, joltage)
+	}
+
+	return joltages, nil
 }
